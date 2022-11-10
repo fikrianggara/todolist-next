3f7dedac69c5e5d75e98c089316497f294fc51b4
fikrianggara <fikriseptriananggara@gmail.com>
2022-11-10 14:55:15 +0700

    add stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  //add codes below to enable app directory as root directory
   experimental: {
     appDir: true,
   },
